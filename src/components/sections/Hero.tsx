a9d4bcdbd6b7fb5217677afea8a3b37ec22228c6
thecrisdasher <dcristian@gmail.com>
2025-05-20 13:00:13 -0500

    final

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -42,7 +42,7 @@ export const Hero = () => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
           <div className="group relative overflow-hidden rounded-3xl">
             <img
-              src="https://videos.openai.com/vg-assets/assets%2Ftask_01jv6hbjzxf1frstc0sapp70q6%2F1747197568_img_0.webp?st=2025-05-14T03%3A14%3A24Z&se=2025-05-20T04%3A14%3A24Z&sks=b&skt=2025-05-14T03%3A14%3A24Z&ske=2025-05-20T04%3A14%3A24Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=8ebb0df1-a278-4e2e-9c20-f2d373479b3a&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=vluy38swT4GKgh9NMEei04TA%2BbutDkFZrVWdOlfhZuI%3D&az=oaivgprodscus"
+              src="/assets/images/amarres.webp"
               alt="Hero image 1"
               width={2350}
               height={2359}
